158e84520ab899e78b5942fad225c1b7d7f39e50
antoniofs23 <af3036@nyu.edu>
2023-12-22 13:14:13 -0500

    added terminal alias+dependencies+sta-up

---
+++ INSTALL.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# an install file that:
+# 1. creates a terminal alias
+# 2. installs python dependencies
+# 3. sets app as a start-up process
+
+# 1. Create a terminal alias in .bashrc
+echo "alias zoom-lens='python3 ~/zoom-lens/main.py &'" >> ~/.bashrc
+
+# 2. Install python dependencies
+pip install -r requirements.txt
+
+# 3. set up as start-up process
+# get username 
+name=$(whoami)
+#build the .desktop file
+cd ~/.config/autostart/
+cat <<EOT >> zoom-lens.desktop
+[Desktop Entry]
+Type=Application
+Path=/home/$name/zoom-lens/
+Exec=python3 main.py
+Terminal=false
+Icon=htop
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=zoom-lens
+Comment[en_US]=runs zoom-lens
+EOT
+
